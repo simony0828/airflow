dd2690d55ff968bc73133a663b6e0c5c366e27b7
simony0828 <simony0828@aim.com>
2023-02-22 19:48:11 -0800

    Update example.py

--- example.py
+++ example.py
@@ -4,6 +4,6 @@ from lib.prestodag import PrestoDAG
 
 p = PrestoDAG(
     dag_id='business_transaction_events_mapping',
-    yaml_file='/files/dags/yaml/business_transaction_events_mapping-Presto-etl.yaml',
+    yaml_file='/files/dags/yaml/events_test-Presto-etl.yaml',
 )
 dag = p.run_dag()
